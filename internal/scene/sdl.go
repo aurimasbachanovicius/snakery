fb22d75e4079052b9deb522bf8492893506e9f09
Aurimas Bachanovicius <aurimas.bachanovicius@metasite.net>
2018-11-26 14:22:38 +0200

    handle error on sdl2 destorying

--- internal/scene/sdl.go
+++ internal/scene/sdl.go
@@ -2,11 +2,14 @@ package scene
 
 import (
 	"fmt"
+
+	"github.com/pkg/errors"
 	"github.com/veandco/go-sdl2/sdl"
 	"github.com/veandco/go-sdl2/ttf"
 )
 
-func PrepareSdl2(width, height int32) (*sdl.Renderer, func(), error) {
+// PrepareSdl2 prepares the sdl2 window.
+func PrepareSdl2(width, height int32) (renderer *sdl.Renderer, destroy func(), erro error) {
 	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
 		return nil, nil, fmt.Errorf("could not init sdl: %v", err)
 	}
@@ -24,9 +27,17 @@ func PrepareSdl2(width, height int32) (*sdl.Renderer, func(), error) {
 		return nil, nil, fmt.Errorf("failed to get surface: %v", err)
 	}
 
-	destroy := func() {
-		r.Destroy()
-		w.Destroy()
+	destroy = func() {
+		if err := r.Destroy(); err != nil {
+			erro = errors.Wrap(err, "could not destroy renderer")
+			return
+		}
+
+		if err := w.Destroy(); err != nil {
+			erro = errors.Wrap(err, "could not destroy window")
+			return
+		}
+
 		ttf.Quit()
 		sdl.Quit()
 	}
